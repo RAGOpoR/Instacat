232695a86fc95251da2bbfd6e7943924026a4dc2
RAGOpoR Sharp <ragopor@me.com>
2015-08-05 22:40:07 +0700

    Add Home template for instagram login.

---
+++ client/controllers/home.js
@@ -0,0 +1,12 @@
+angular.module('Instacat')
+  .controller('HomeCtrl', function($scope) {
+
+    $scope.isAuthenticated = function() {
+      // check if logged in
+    };
+
+    $scope.linkInstagram = function() {
+      // connect email account with instagram
+    };
+
+  });
